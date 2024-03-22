7903e8afc482aa88e2f6bf2a387348437458b092
Heshan <mhsandamal1@gmail.com>
2024-03-22 20:55:23 +0000

    Fixed build issue

--- frontend/src/components/__tests__/MovieList.test.js
+++ frontend/src/components/__tests__/MovieList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import axios from 'axios';
-import MovieList from '../MovieList';
+import MovieList from "../MovieList";
 
 jest.mock('axios');
 
